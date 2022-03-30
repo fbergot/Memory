13d5801b94acd9b7859bc431505e48584abd5829
fbergot <florian.bergot564@gmail.com>
2022-03-30 16:51:42 +0200

    update app.js + html (score & vies)

--- src/app.js
+++ src/app.js
@@ -6,21 +6,39 @@ import {
    setAddEventListener,
 } from "./assets/JS/game/game";
 
-const board = document.querySelector("#container");
+const $ = (tag, isAll) => {
+   return isAll ? document.querySelectorAll(tag) : document.querySelector(tag);
+};
+
+const board = $("#container");
+const score = $("#score");
+const life = $("#life");
 
 const dataCards = buildArrayCards(8);
 const shuffleDataCards = shuffleArray(dataCards);
 
 setBoard(board, shuffleDataCards);
 
-const cards = document.querySelectorAll(".card img");
+const cards = $(".card img", true);
 
-const state = [];
+let state = [];
 
-const $ = (event) => {
+const handleClickImg = (event) => {
+   if (state[0]) {
+      if (Number(state[0].dataset.id) === Number(event.target.dataset.id)) {
+         alert("gagner");
+         state = [];
+         event.target.classList.remove("initCard");
+      } else {
+         window.setTimeout(() => {
+            state[0].classList.add("initCard");
+            state[1].classList.add("initCard");
+         }, 1000);
+         state = [];
+      }
+   }
+   state.push(event.target);
    event.target.classList.remove("initCard");
-   state.push(Number(event.target.dataset.id));
-   console.log(state);
 };
 
-setAddEventListener(cards, $);
+setAddEventListener(cards, handleClickImg);
