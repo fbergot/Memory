e2e4e8516e9d854be23b3ed43cf4a7c4e0357852
fbergot <florian.bergot564@gmail.com>
2022-03-30 23:30:41 +0200

    refacto handleClickImg & creation of State class for gameState + refacto in buildArrayCards in game.js

--- src/assets/JS/utils/utils.js
+++ src/assets/JS/utils/utils.js
@@ -1,9 +1,18 @@
+import getImgPath from "../utils/dictionnaryImg";
 /**
  * Return an or many HTML elements
  * @param {string} tag
  * @param {true | undefined} isAll
  * @return {HTMLElement | NodeList}
  */
-export function $(tag, isAll) {
-   return isAll ? document.querySelectorAll(tag) : document.querySelector(tag);
+export function $(tag, All) {
+   return All ? document.querySelectorAll(tag) : document.querySelector(tag);
+}
+
+/**
+ * @param {number} index
+ * @return {{id: number, path: string}}
+ */
+export function buildDataOfCard(index) {
+   return { id: index, path: getImgPath(index) };
 }
